f23ef4e6ca709711c94f09c18b2c3dd670cbda29
Prabesh Upreti <prabeshupreti29@gmail.com>
2025-09-01 16:09:46 +0545

    new file committed

--- menu.py
+++ menu.py
@@ -1,28 +1,79 @@
-menu ={
-    'Pizza':150,
-    'Pasta':50,
-    'Salad':100,
-    'Coffee':60,
-    'Burger':90
-}
+# menu ={
+#     'Pizza':150,
+#     'Pasta':50,
+#     'Salad':100,
+#     'Coffee':60,
+#     'Burger':90
+# }
 
-print("Welcome to Turning Cafe")
-print("Pizza :Rs.150\nPaste:Rs.50\nSalad:Rs.10\nCoffee:Rs.60\nBurger:Rs.90")
+# print("Welcome to Turning Cafe")
+# for item,price in menu.items():
+#     print(f"{item}:RS.{price}")
 
-order_total = 0
-item = input("enter the item you want to have :")
-if item in menu:
-    order_total = menu[item]
-    print(f"Your {item} has been sucessfully added to your order.")
-else:
-    print('Please order something else we can serve you')
 
-another_order = input("Do you want to add another item(Yes/No)")
-if another_order=='Yes':
-    item2 = input("enter the another item")
-    if item2 in menu:
-        order_total = menu[item2]
-        print(f"Your {item2} has added")
-    else:
-        print('Please order something else we can serve you')
-print(f"The total amount is to be paid is {order_total}")
+# order_total = 0
+
+# while True:
+#     item = input("enter the item you want to take: ")
+#     if item in menu:
+#         order_total+=menu[item]
+#         print(f"Your {item} has beed added")
+#         print(f"The total amount to be paid is {order_total}")
+#     else:
+#         print("sorry we do not serve this item.please order something else")
+
+#     another_order = input("Do you want to add another item (Yes/No)")
+#     if another_order!="Yes":
+#         break
+
+
+# using OOP concept
+
+class Cafe:
+    def __init__(self):
+        self.menu={
+            'Pasta':100,
+            "Milk Tea":50,
+            "Milk Coffee":80,
+            "Momo":150,
+            "Burger":200
+        }
+
+        self.order_total = 0
+        self.order_List = []
+
+    def show_menu(self):
+        print("-----Welcome to GoloCafe------------------")
+        print("Menu:")
+        for item,price in self.menu.items():
+            print(f"{item}:Rs.{price}")
+
+    
+    def take_order(self):
+        while True:
+            item1 = input("Enter the item you want to order: ")
+            if item1 in self.menu:
+                self.order_total+=self.menu[item1]
+                self.order_List.append(item1)
+                print(f"Your {item1} has been added")
+                
+
+            else:
+                print("sorry we do not serve this item.please order something else")
+
+            another_order = input("Do you want to add another item (Yes/No)?")
+            if another_order!='Yes':
+                break
+
+    def Show_bill(self):
+        print('\n---------Show bill----------')
+        for item in self.order_List:
+            print(f"{item} :{self.menu[item]}")
+        print(f"Total Amount to be paid: Rs.{self.order_total}")
+my_cafe = Cafe()
+print(my_cafe.show_menu())
+print(my_cafe.take_order())
+print(my_cafe.Show_bill())
+
+
+
